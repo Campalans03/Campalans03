bc122c922f8d10b916a4afc3931a34544c310647
Campalans <101204872+Campalans03@users.noreply.github.com>
2025-01-31 15:26:32 +0100

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,12 +1,16 @@
-# update_readme.py
 with open('README.md', 'r') as f:
     readme = f.read()
 
 with open('game_board.txt', 'r') as f:
     game_board = f.read()
 
-# Reemplazar la sección del tablero en el README
+with open('game_state.json', 'r') as f:
+    game_state = json.load(f)
+    score = game_state["score"]
+
+# Reemplazar marcadores
 updated_readme = readme.replace('{{ game_board }}', game_board)
+updated_readme = updated_readme.replace('{{ score }}', str(score))
 
 with open('README.md', 'w') as f:
     f.write(updated_readme)
